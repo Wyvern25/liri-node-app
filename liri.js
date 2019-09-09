de4b81563638c1b9d431f296ac5f806a0c87fe1f
Wyvern <Wyvern25@users.noreply.github.com>
2019-09-08 23:48:34 -0700

    spotifiy api integration complete. markdown and readme to go

--- liri.js
+++ liri.js
@@ -3,9 +3,9 @@ var moment = require('moment');
 const axios = require('axios');
 var fs = require('fs');
 var keys = require("./keys.js");
-/*var spotifyKey = new Spotify
-    (keys.spotify);
-*/
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
+
 var functionType = process.argv[2];
 var parameterArray = process.argv.slice(3)
 var parameter = parameterArray.join(' ');
@@ -14,20 +14,40 @@ function missingInfo() {
     var defaultCase = fs.readFileSync('./defaultCases.txt', 'utf8');
     defaultArray = defaultCase.split('\r\n');
 }
-/*function spotify() {
-    var content = fs.readFileSync('./test.txt', 'utf8');
-    console.log('song ');
+function spot() {
+    //var content = fs.readFileSync('./test.txt', 'utf8');
+    //console.log('song ');
+    parameter = parameterArray.join(' ');
     if (process.argv[3] === undefined) {
         missingInfo();
         var parameter = defaultArray[1];
         console.log('No song entered. Default: Down with the Sickness');
         console.log('');
-    } else {
-        console.log(parameter);
     }
+
+    //console.log(parameterArray);
+
+    //console.log(parameter);
+
+
+
+    spotify.search({ type: 'track', query: parameter }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+
+        //console.log(data.tracks);
+        console.log('Song name: ' + data.tracks.items[0].name);
+        console.log('Artists: ' + data.tracks.items[0].artists[0].name);
+        console.log('Preview URL: ' + data.tracks.items[0].preview_url);
+        var myJSON = JSON.stringify(data.tracks.items[0].album.name);
+        console.log('Album: ' + myJSON);
+    });
+
+
     fs.writeFileSync('./test.txt', parameter, 'utf8');
 }
-*/
+
 if (functionType == 'concert') {
     var content = fs.readFileSync('./test.txt', 'utf8');
 
@@ -36,7 +56,8 @@ if (functionType == 'concert') {
     if (process.argv[3] === undefined) {
         missingInfo();
         var parameter = defaultArray[2];
-        console.log('No artist entered. Default: Disturbed');
+        console.log('No artist entered');
+        console.log('Default: Disturbed');
         console.log('');
     }
     axios.get("https://rest.bandsintown.com/artists/" + parameter + "/events?app_id=codingbootcamp")
@@ -58,14 +79,15 @@ if (functionType == 'concert') {
             }
         });
     fs.writeFileSync('./test.txt', parameter, 'utf8');
-}/* else if (functionType == 'spotify') {
-    spotify();
-}*/ else if (functionType == 'movie') {
+} else if (functionType == 'spotify') {
+    spot();
+} else if (functionType == 'movie') {
 
     if (process.argv[3] === undefined) {
         missingInfo();
         var parameter = defaultArray[3];
-        console.log('No movie entered. Default: The Game');
+        console.log('No movie entered');
+        console.log('Default: The Game');
         console.log('');
     }
     axios.get('http://www.omdbapi.com/?t=' + parameter + '&apikey=trilogy')
@@ -86,25 +108,15 @@ if (functionType == 'concert') {
 
 
 
-        })
-    /*var content = fs.readFileSync('./test.txt', 'utf8');
-    console.log('movie ');
-    if (parameter === undefined) {
-        missingInfo();
-        var parameter = defaultArray[3];
-        console.log(parameter);
-    } else {
-        console.log(parameter);
-    }
+        });
 
-    fs.writeFileSync('./test.txt', parameter, 'utf8');*/
 } else { //default
     var content = fs.readFileSync('./defaultCases.txt', 'utf8');
 
-    console.log('default: Spotify; Down with the Sickness');
-    console.log('you did not enter a valid choice');
+    console.log('You did not enter a valid choice');
+    console.log('Default: Spotify');
     console.log('');
-    //spotify();
+    spot();
     //console.log(content);
 
     //var newContent = content - parameter;
